1f68fad741a74bf24b1d360c3b6558e5baf02cbe
Musrad <musradabdul007@gmail.com>
2026-07-09 11:12:01 +0530

    Created user.jsx

---
+++ src/data/users.jsx
@@ -0,0 +1,9 @@
+const users = [
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Smith", email: "jane@example.com" },
+  { id: 3, name: "Michael Brown", email: "michael@example.com" },
+  { id: 4, name: "Emily Davis", email: "emily@example.com" },
+  { id: 5, name: "David Wilson", email: "david@example.com" },
+];
+
+export default users;
