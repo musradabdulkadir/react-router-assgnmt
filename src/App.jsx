82643a4170fbcc9d627d59fa45d466ad139a6678
Musrad <musradabdul007@gmail.com>
2026-07-09 12:08:45 +0530

    Page.css issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./pages/pages.css";
+import "./pages/Pages.css";
 import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
